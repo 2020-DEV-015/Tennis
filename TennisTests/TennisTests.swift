ffaa853ad69e96b8d97cc379c40a73ea75b65b48
2020 Dev 015 <2020.dev.015@gmail.com>
2020-01-21 00:56:14 +0530

    Added test cases for the player A  winning first three balls

--- TennisTests/TennisTests.swift
+++ TennisTests/TennisTests.swift
@@ -3,11 +3,40 @@ import XCTest
 
 class TennisTests: XCTestCase {
     
+    var playerA: Player!
+    var playerB: Player!
+    var tennis: Tennis!
+    
+    override func setUp() {
+        super.setUp()
+        playerA = Player(name: "Player A")
+        playerB = Player(name: "Player B")
+        tennis = Tennis(playerA: playerA, playerB: playerB)
+    }
+    
     func testNewGameShouldReturnLoveAll() {
-        let playerA = Player(name: "Player A")
-        let playerB = Player(name: "Player B")
-        let tennis = Tennis(playerA: playerA, playerB: playerB)
         let expectedScore = "Love,All"
+        XCTAssertEqual(expectedScore, tennis.getScore())
+    }
+
+    func testPlayerAWinsFirstBall() {
+        playerA.scored()
+        let expectedScore = "Fifteen,Love"
+        XCTAssertEqual(expectedScore, tennis.getScore())
+    }
+    
+    func testPlayerAWins_2_0() {
+        playerA.scored()
+        playerA.scored()
+        let expectedScore = "Thirty,Love"
+        XCTAssertEqual(expectedScore, tennis.getScore())
+    }
+    
+    func testPlayerAWins_3_0() {
+        playerA.scored()
+        playerA.scored()
+        playerA.scored()
+        let expectedScore = "Forty,Love"
         XCTAssertEqual(expectedScore, tennis.getScore())
     }
 }
