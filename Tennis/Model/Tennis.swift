23ec9718d9edf500449678f7e6bdef0370e3ad53
2020 Dev 015 <2020.dev.015@gmail.com>
2020-01-20 23:32:52 +0530

    1. Added player a and player b variable in the tennis class for keeping track of the player and the scores 2. Added initializers for the tennis class which injects the player objects 3. Updated the new game should return love, all test cases with the player objects

--- Tennis/Model/Tennis.swift
+++ Tennis/Model/Tennis.swift
@@ -1,4 +1,14 @@
 class Tennis {
+    
+    private var playerA: Player
+    private var playerB: Player
+    private let scores: [String] = ["Love", "Fifteen", "Thirty", "Forty"]
+    
+    init(playerA: Player, playerB: Player) {
+        self.playerA = playerA
+        self.playerB = playerB
+    }
+
     func getScore() -> String {
         return "Love, All"
     }
--- TennisTests/TennisTests.swift
+++ TennisTests/TennisTests.swift
@@ -4,7 +4,9 @@ import XCTest
 class TennisTests: XCTestCase {
     
     func testNewGameShouldReturnLoveAll() {
-        let tennis = Tennis()
+        let playerA = Player(name: "Player A")
+        let playerB = Player(name: "Player B")
+        let tennis = Tennis(playerA: playerA, playerB: playerB)
         let expectedScore = "Love, All"
         XCTAssertEqual(expectedScore, tennis.getScore())
     }
